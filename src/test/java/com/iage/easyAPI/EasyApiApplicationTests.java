1b106ba8738f3f8e4363965c4dac5ac684f99f1e
kathan11 <kspathak@icloud.com>
2024-05-16 18:39:38 -0700

    Added project files

---
+++ src/test/java/com/iage/easyAPI/EasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iage.easyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
